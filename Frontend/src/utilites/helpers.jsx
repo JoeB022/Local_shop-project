b481a1a1258fe9599e9aa978b1afa96473d2d8da
maliikabdi <lildarwish13158@gmail.com>
2025-03-01 17:17:57 +0300

    Delete Frontend directory

--- Frontend/src/utilites/helpers.jsx
+++
@@ -1,9 +0,0 @@
-export const formatUserName = (name) => {
-    if (!name) return '';
-    return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
-  };
-  
-  export const isAuthenticated = (user) => {
-    return user && user.isLoggedIn;
-  };
-  
